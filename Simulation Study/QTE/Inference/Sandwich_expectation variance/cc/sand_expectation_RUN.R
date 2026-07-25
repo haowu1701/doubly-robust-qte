5c2eae090a08cb17574060fce4c1b23b776b2860
haowu1701 <143580617+haowu1701@users.noreply.github.com>
2026-07-25 15:24:06 -0400

    Add files via upload

---
+++ Simulation Study/QTE/Inference/Sandwich_expectation variance/cc/sand_expectation_RUN.R
@@ -0,0 +1,36 @@
+library(Matrix)
+library(MASS)
+library(rms)
+library(dplyr)
+library(tidyr)
+
+source("Helper_functions.R")
+source("variance_helpers.R")
+load("nsim1000_data_n1000.RData")
+load("true_values.RData")
+truth_q1  = m_Y1_0.5
+truth_q0  = m_Y0_0.5
+truth_qte = QTE_0.5_true
+
+p_star= 0.5; scenario = "cc"; n_run  = length(data_list)
+test_data = generate_data( n = 1000,alpha1 = 0.5, alpha2 = 0.35, beta1 = -2, beta2 = 3, delta = 2 )
+dd = datadist(test_data); options(datadist = "dd")
+
+
+res_all = run_sandwich_expectation_on_datalist(
+  data_list = data_list,
+  scenario  = scenario,   
+  p_star = p_star,
+  n_run = n_run
+)
+
+
+
+out = summarize_sandwich_results(
+  res_df = res_all,
+  truth_q1  = truth_q1,
+  truth_q0  = truth_q0,
+  truth_qte = truth_qte
+)
+out$summary
+save(res_all, file = paste0("sandwich_results_expetation_", scenario, ".RData"))
